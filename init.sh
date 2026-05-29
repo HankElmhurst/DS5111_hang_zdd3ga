89876a4055e2f64539f34eb282df4755aa616144
Hang <zdd3ga@virginia.edu>
2026-05-29 02:38:12 +0000

    saving init scripts, makefile, and requirements

---
+++ init.sh
@@ -0,0 +1,4 @@
+sudo apt update # To bring VM snapshot up to date with package versions
+sudo apt install make -y # To use makefiles
+sudo apt install python3.14-venv -y # To create python virtual environments
+sudo apt install tree # a usefull tool for listing files in tree form
---
+++ init_git_creds.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/bash
+
+USER="zdd3ga@virginia.edu"
+NAME="Hang Yu"
+
+git config --global --list
+
+git config --global user.email ${USER} 
+git config --global user.name  ${NAME} 
+
+git config --global --list
